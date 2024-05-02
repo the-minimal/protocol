6d1ed2b31c2c93efda852d92708c583768467942
Yamiteru <yamiteru@icloud.com>
2024-05-02 22:03:50 +0200

    feat: get coverage from tests

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+    plugins: [tsconfigPaths()],
+    test: {
+        isolate: true,
+        coverage: {
+            provider: "v8",
+            include: ["tests/*.test.ts"],
+            exclude: ["src"],
+            reporter: ["text", "html", "clover", "json", "json-summary"]
+        }
+    },
+});
